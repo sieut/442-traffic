0e7c29fc5658629952cd873bea1bb5b7bde30287
Sieu Tsoi <sieut@umich.edu>
2019-04-07 09:05:12 -0400

    Update to script and cam_id

--- collect.py
+++ collect.py
@@ -1,10 +1,12 @@
 import urllib.request
 import os
+import sys
 import time
 
 DATA_DIR = "data/%s"
 IM_URL = "http://giaothong.hochiminhcity.gov.vn/render/ImageHandler.ashx?id=%s&t=0"
 IM_NAME = "im_%s"
+SLEEP = 60
 
 def main():
     with open("cam_id", "r") as f:
@@ -18,13 +20,17 @@ def main():
 
     while True:
         for id in cam_ids:
-            im = urllib.request.urlopen(IM_URL % id).read()
-            with open("%s/%s" % (DATA_DIR % id,
-                                 IM_NAME % im_counts[id]),
-                      "wb") as im_file:
-                im_file.write(im)
-            im_counts[id] += 1
-        time.sleep(15)
+            try:
+                im = urllib.request.urlopen(IM_URL % id).read()
+                with open("%s/%s" % (DATA_DIR % id,
+                                     IM_NAME % im_counts[id]),
+                          "wb") as im_file:
+                    im_file.write(im)
+                im_counts[id] += 1
+            except:
+                print("Failed to collect image", file=sys.stderr)
+
+        time.sleep(SLEEP)
 
 if __name__ == "__main__":
     main()
